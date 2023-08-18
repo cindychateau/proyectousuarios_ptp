1ab364114016ddffef9439ad4bc872f04f4663e3
Cynthia Castillo <ca.castillo@gmail.com>
2023-08-17 18:22:07 -0600

    JSTL

--- src/main/java/com/codingdojo/cynthia/controladores/ControladorUsuarios.java
+++ src/main/java/com/codingdojo/cynthia/controladores/ControladorUsuarios.java
@@ -1,6 +1,9 @@
 package com.codingdojo.cynthia.controladores;
 
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,6 +12,26 @@ public class ControladorUsuarios {
 	@GetMapping("/")
 	public String index() {
 		return "index.jsp";
+	}
+	
+	@GetMapping("/usuarios")
+	public String usuarios(Model model) {
+		
+		//String titulo="Título para mis Usuarios"
+		model.addAttribute("titulo", "Título para mis Usuarios");
+		
+		String usuarios[] = {"Elena de Troya", "Juana de Arco", "Pablo Picasso"};
+		model.addAttribute("listaUsuarios", usuarios);
+		
+		HashMap<String, String> paises = new HashMap<String, String>();
+		
+		paises.put("Mexico", "CDMX");
+		paises.put("Estados Unidos", "Washington DC");
+		paises.put("El Salvador", "San Salvador");
+		
+		model.addAttribute("paises", paises);
+		
+		return "usuarios.jsp";
 	}
 	
 }
